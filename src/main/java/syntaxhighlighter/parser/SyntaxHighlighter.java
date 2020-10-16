fa4565e05f0d4bf78ae09118b16b4a4e6293934c
Thomas Hirsch <thomas@k15t.com>
2020-10-16 14:29:02 +0200

    IEXP-3908: Clean up imports

--- src/main/java/syntaxhighlighter/parser/SyntaxHighlighter.java
+++ src/main/java/syntaxhighlighter/parser/SyntaxHighlighter.java
@@ -27,7 +27,6 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.swing.text.Segment;
 import syntaxhighlighter.brush.Brush;
 import syntaxhighlighter.brush.RegExpRule;
 
